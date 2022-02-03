e7f3106fdceeba3e45b35a640197481f8e8af2bf
jankunasm <jankunasmatas11@gmail.com>
2022-02-02 18:11:33 -0600

    Commit 2.

---
+++ review.py
@@ -0,0 +1,23 @@
+num1 = 5
+
+num2 = 10
+
+if num2 == num1:
+    print("Equal Values")
+else:
+    print("Not Equal Values")
+
+if num2 >= num1:
+    print("Num2 is greater or equal")
+else:
+    print("Num1 is less")
+
+char_name = 'Max Payne'
+
+if "Max" in char_name:
+    print("The character who lost everything...")
+
+sega_char = "Sonic"
+
+if 'a' not in sega_char:
+    print("a is not here... Mario all day")
---
+++ testerr.py
@@ -0,0 +1,18 @@
+# m = int(input())
+# n = str(input() + 00)
+
+
+# print(n)
+
+
+sentence = 'Hello Perfect World'
+col = 5
+
+for char in sentence:
+    if char in sentence:
+        print(sentence[0:(col)])
+    #     print(sentence[0:(col)])
+    # else:
+    #     False
+
+
